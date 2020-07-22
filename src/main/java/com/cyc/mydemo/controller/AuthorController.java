3ca1bce1685555e817c57512627665dbffcb20ea
chenyuchuan <917148007@qq.com>
2020-07-22 15:13:07 +0800

    补充 user和book的 controller

---
+++ src/main/java/com/cyc/mydemo/controller/AuthorController.java
@@ -0,0 +1,21 @@
+package com.cyc.mydemo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author cyc
+ * @since 2020-07-22
+ */
+@Controller
+@RequestMapping("/author")
+public class AuthorController {
+
+}
+
